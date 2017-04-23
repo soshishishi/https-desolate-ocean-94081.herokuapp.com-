f9dae1c9ee8ef245a85956a21294c9a1ddc12e66
Natth <my_darkstyle@hotmail.com>
2017-04-23 17:14:08 +0700

    add index.php

--- index.php
+++ index.php
@@ -19,8 +19,13 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => "ไม่เข้าใจ"
 			];
+      if (text=="hi") {
+        $messages = [
+  				'type' => 'text',
+  				'text' => "ชิมิ"
+      }
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
